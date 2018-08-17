05ac10dc8a702cd28ef84c21072126879f9bdd5e
derek <you@example.com>
2018-08-17 00:55:06 -0400

    Slight updates + Addition of extras
    Extras go with the Appendix, and the slight updates match the
    reformulated version of CanCA.

--- TCATA/analyses.R
+++ TCATA/analyses.R
@@ -1,12 +1,6 @@
 load('tcata.sim.data.rda')
 source('helpers.R')
 
-# Y <- DATA
-# X.time <- makeNominalData(TIME)
-# X.products <- makeNominalData(PRODUCTS)
-# X.products.time <- makeNominalData(PRODUCTS.and.TIME)
-
-
 Y <- tcata.sim.data$DATA
 X.products <- makeNominalData(as.matrix(tcata.sim.data$DESIGNS[,1]))
 X.time <- makeNominalData(as.matrix(tcata.sim.data$DESIGNS[,2]))
@@ -23,9 +17,15 @@ products <- c("PA", "PB", "PC", "PD")
 standard.ca.res <- epCA(Y,DESIGN=X.products,make_design_nominal = F,graphs=F)
 
 ## Canonical CA
-WI <- t(X.products.time) %*% diag(rowSums(Y)) %*% X.products.time
+WI <- diag(rowSums(Y))
+ 	WI <- WI / sum(WI)
+WI.star <- t(X.products.time) %*% WI %*% X.products.time
+	WI.star <- WI.star / sum(WI.star)
 WJ <- diag(colSums(Y))
-can.ca.res <- simple.can.ca(t(X.products.time) %*% Y,WI,WJ)
+	WJ <- WJ / sum(WJ)
+R <- t(X.products.time) %*% Y
+	Or <- R / sum(R)
+can.ca.res <- simple.can.ca(Or, WI.star, WJ)
 
 
 ## Conditional CA
